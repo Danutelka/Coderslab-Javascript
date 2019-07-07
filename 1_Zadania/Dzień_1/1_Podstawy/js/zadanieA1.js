9f8860edcb5cadf419fd59a77fd351cf88fe195c
Danuta <kawecka.d@gmail.com>
2019-07-07 18:59:59 +0200

    dzien1-1-A1-A2

--- 1_Zadania/Dzień_1/1_Podstawy/js/zadanieA1.js
+++ 1_Zadania/Dzień_1/1_Podstawy/js/zadanieA1.js
@@ -1,6 +1,24 @@
 function mathInfo() {
     var randomNumber = Math.floor(Math.random() * 100);
+    console.log('Wylosowana liczba to:', randomNumber)
     //poniżej napisz rozwiązanie zadania
+    if (randomNumber % 2 === 0) {
+        console.log('Liczba jest parzysta');
+    } else {
+        console.log('Liczba jest nieparzysta');
+    }
+    // petla for zaczyna od wylosowanej liczny i schodzi w dól do 0
+    var arr = []; // new_Array () zamiast []; tablica -js, list - python
+    for (var i =randomNumber; i>0; i--) {
+        if (randomNumber % i === 0) {
+            arr.push(i);
+        }
+    }
+    console.log('Dzielniki wylosowanej liczby:', arr.join(','));
+
+    var wykladnik = randomNumber % 5;
+
+    console.log('potęga o wykładniku', wykladnik, 'wynosi', randomNumber ** wykladnik)
 }
 
 //to wywołanie ma spowodować wyświetlenie w konsoli informacji z zadania
--- 1_Zadania/Dzień_1/1_Podstawy/js/zadanieA2.js
+++ 1_Zadania/Dzień_1/1_Podstawy/js/zadanieA2.js
@@ -6,12 +6,12 @@
 function myEval(a, b, operation) {
     var result = 0;
     switch(operation) {
-        case'+': result = a + b;
-        case'-': result = a - value.b;
-        case'/': result = a / b;
-        case'*': result = a * b;
-        case'%': result = a % b;
-        case'*': result = Math.pow(a, b);
+        case'+': result = a + b; break;
+        case'-': result = a - b; break;
+        case'/': result = a / b; break;
+        case'*': result = a * b; break;
+        case'%': result = a % b; break;
+        case'^': result = Math.pow(a, b); break;
     }
     return result;
 }
@@ -20,3 +20,6 @@ console.log(" 2 + 4 = " + myEval(2, 4, "+"));
 console.log(" 5 - 2 = " + myEval(5, 2, "-"));
 console.log(" 4 ^ 8 = " + myEval(4, 8, "^"));
 //poniżej dopisz pozostałe przypadki działań jakie ma obsługiwać funkcja
+console.log(" 4 % 8 = " + myEval(4, 8, "%"));
+console.log(" 4 / 8 = " + myEval(4, 8, "/"));
+console.log(" 2 * 4 = " + myEval(2, 4, "*"));
